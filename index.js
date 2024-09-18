273b5ba3a5a0a8134cc37795af03a80d71c4fc54
CarlosKokouAtedzi <kokou.atedzi26@compscihigh.org>
2024-09-18 17:12:49 +0000

    all changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Task 1
 // Print your name
-console.log(Kokou)
+console.log("Kokou")
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+console.log("Candy")
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+console.log("Rod Wave")
 // Stage and commit this change with an appropriate message
 // Go to more.js
